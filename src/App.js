eae88610ba02230a92a46691d730ef381752ddaf
nutlope <he55@drexel.edu>
2021-12-22 23:06:14 -0500

    added google analytics tracking

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import Routes from './Routes';
+import ReactGA from 'react-ga';
 
 import 'react-lazy-load-image-component/src/effects/opacity.css';
 import 'leaflet/dist/leaflet.css';
 import 'assets/css/index.css';
-
 import 'swiper/css/swiper.min.css';
 import 'aos/dist/aos.css';
 
 const browserHistory = createBrowserHistory();
 console.log('browserHistory', browserHistory);
+
+const TRACKING_ID = 'G-T6NVVJXMZ8';
+ReactGA.initialize(TRACKING_ID);
+
 const App = () => {
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname);
+  }, []);
   return (
     <Router history={browserHistory}>
       <Routes />
