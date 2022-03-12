7bd315a6d5232f3e91e66da6a9ed8515fead4ea5
nutlope <he55@drexel.edu>
2022-03-12 12:31:44 -0500

    added intermcom

--- src/App.js
+++ src/App.js
@@ -3,6 +3,11 @@ import { Router } from 'react-router-dom';
 import { createBrowserHistory } from 'history';
 import Routes from './Routes';
 import ReactGA from 'react-ga';
+import {
+  load as loadIntercom,
+  boot as bootIntercom,
+  update as updateIntercom,
+} from './intercom';
 
 import 'react-lazy-load-image-component/src/effects/opacity.css';
 import 'leaflet/dist/leaflet.css';
@@ -11,9 +16,15 @@ import 'swiper/css/swiper.min.css';
 import 'aos/dist/aos.css';
 
 const browserHistory = createBrowserHistory();
-console.log('browserHistory', browserHistory);
+
+browserHistory.listen(location => {
+  // Calls Intercom('update') on every page change
+  updateIntercom();
+});
 
 const App = () => {
+  loadIntercom();
+  bootIntercom();
   ReactGA.initialize('UA-215866314-1');
   ReactGA.pageview(window.location.pathname + window.location.search);
   return (
